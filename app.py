131a493546ff28c685878abb23bb73f65243a4f9
SteliosMacM2 <494002@student.saxion.nl>
2024-12-13 13:26:41 +0200

    prefinal

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
 from openai import OpenAI
 import os
 
 import streamlit as st
 
 from openai import OpenAI
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"),)
 
 prompt = st.text_area("Instructions",
